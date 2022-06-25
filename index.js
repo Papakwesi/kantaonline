c7067709e936d3e76b7187dc6aefe471edd7a3ec
papakwesi <93794323+Papakwesi@users.noreply.github.com>
2022-06-25 23:30:33 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+var firstCount = document.querySelector(".view-count1").innerHTML
+var secondCount = document.querySelector(".view-count2").innerHTML
+var thirdCount = document.querySelector(".view-count3").innerHTML
+var forthCount = document.querySelector(".view-count4").innerHTML
+var fifthCount = document.querySelector(".view-count5").innerHTML
+var sixthCount = document.querySelector(".view-count6").innerHTML
+var seventhCount = document.querySelector(".view-count7").innerHTML
+var eightCount = document.querySelector(".view-count8").innerHTML
+var ninthCount = document.querySelector(".view-count9").innerHTML
+
+
+// for (var i = 0; i < viewBtn.length; i++) {
+// 	document.querySelectorAll(".btn-group")[i].addEventListener("click", function(){
+// 		document.querySelector(".card-1").innerHTML = viewCount ++
+// 	})
+// }
+
+document.querySelector(".btn-group1").addEventListener("click", function(){
+	document.querySelector(".card-1").innerHTML = firstCount ++
+})
+document.querySelector(".btn-group2").addEventListener("click", function(){
+	document.querySelector(".card-2").innerHTML = secondCount ++
+})
+document.querySelector(".btn-group3").addEventListener("click", function(){
+	document.querySelector(".card-3").innerHTML = thirdCount ++
+})
+document.querySelector(".btn-group4").addEventListener("click", function(){
+	document.querySelector(".card-4").innerHTML = forthCount ++
+})
+document.querySelector(".btn-group5").addEventListener("click", function(){
+	document.querySelector(".card-5").innerHTML = fifthCount ++
+})
+document.querySelector(".btn-group6").addEventListener("click", function(){
+	document.querySelector(".card-6").innerHTML = sixthCount ++
+})
+document.querySelector(".btn-group6").addEventListener("click", function(){
+	document.querySelector(".card-6").innerHTML = seventhCount ++
+})
+document.querySelector(".btn-group6").addEventListener("click", function(){
+	document.querySelector(".card-6").innerHTML = eightCount ++
+})
+document.querySelector(".btn-group9").addEventListener("click", function(){
+	document.querySelector(".card-9").innerHTML = ninthCount ++
+})
+
+
+
+
